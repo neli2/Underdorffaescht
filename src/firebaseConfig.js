f705235475b0de919b819826741b00b4b65d37f3
TecFoxGmbH <info@tecfox.net>
2026-03-31 21:42:43 +0200

    2026

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyDXROvbOlivAHCc3qClSBMTA4qb4fz886w",
-  authDomain: "underdorffaest123.firebaseapp.com",
-  projectId: "underdorffaest123",
-  storageBucket: "underdorffaest123.firebasestorage.app",
-  messagingSenderId: "861002001632",
-  appId: "1:861002001632:web:2d0e3bddcd79465d8f7485"
+  apiKey: "AIzaSyAwispXvMfKVD5TEpUYjXhfasK2S1XiDnM",
+  authDomain: "underdorffaescht.firebaseapp.com",
+  projectId: "underdorffaescht",
+  storageBucket: "underdorffaescht.firebasestorage.app",
+  messagingSenderId: "891824361865",
+  appId: "1:891824361865:web:ba86788fdc72cdfaaad158"
 };
 
 export default firebaseConfig;
